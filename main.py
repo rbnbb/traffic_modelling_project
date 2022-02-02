8c041900a4ae9703222098b8a428db5033fa8538
rbnbb <79351111+rbnbb@users.noreply.github.com>
2022-02-02 14:08:37 +0100

    project subject pdf file and schematic main.py
    main.py has mostly commentaries and most functionality should be implemented in separate files and imported to main.py

---
+++ main.py
@@ -0,0 +1,17 @@
+import numpy as np
+
+#set initial conditions
+"""Initial conditions should specify the average velocity and density in each cell"""
+##set model size and precision
+N=10 #number of cells
+dx=2 #size of a cell in meters
+dt=2 #time step
+##set average velocity and density
+v0=np.array 40e3/60 #initial uniform velocity of 40 km/h expressed in meters per minute
+
+
+#solve equations 
+
+
+#plot and analyse data
+
